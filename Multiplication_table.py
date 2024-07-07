a82c90f256113480855130c36562da8e640d97b3
ANIK CHOWDHURY <109497220+anik-chowdhury-002@users.noreply.github.com>
2024-07-07 08:52:50 +0530

    Add files via upload

---
+++ Multiplication_table.py
@@ -0,0 +1,5 @@
+#using for loop
+num = int(input("Enter the upper range : "))
+mul = int(input("Enter a number from which you want to multiply"))
+for i in range(1,num+1):
+    print(mul,"*",i,"=",mul*i)
